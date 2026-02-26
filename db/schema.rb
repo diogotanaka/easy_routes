453a9e99df8e5bb9d51956b0b0c9d1cf0976e151
Beatriz Barguil <beatrizz.barguil@gmail.com>
2026-02-26 20:48:18 -0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 3) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_21_212521) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
-  create_table "trips", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "origin"
-    t.string "destination"
-    t.date "start_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_trips_on_user_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.integer "trip_id"
-    t.string "role"
+  create_table "chats", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["trip_id"], name: "index_messages_on_trip_id"
   end
 
-  add_foreign_key "trips", "users"
-  add_foreign_key "messages", "trips"
+  create_table "messages", force: :cascade do |t|
+    t.text "request"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.text "destination_name"
+    t.text "initial_location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
