9194efdfa60fc38b192f35cb4aba525f138ff0ec
Thais Almeida Gai <thais.gabriel@gmail.com>
2026-02-26 22:42:33 -0300

    Add create no  messages_controller

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -4,3 +4,26 @@ def index
     @messages = @trip.messages.order(:created_at)
     @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)  # cria mensagem com content do form
+    @message.trip_id = @trip.id
+
+  if @message.save                         # tenta salvar no banco
+    redirect_to trip_messages_path(@trip)
+  else
+    @messages = @trip.messages.order(:created_at)
+    render :index, status: :unprocessable_entity
+  end
+end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
