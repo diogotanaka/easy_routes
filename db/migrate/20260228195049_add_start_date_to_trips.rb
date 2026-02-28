eb1ad17fc33d2c4ad2b2dbd3ccba9b199d0914ba
Beatriz Barguil <beatrizz.barguil@gmail.com>
2026-02-28 17:21:50 -0300

    atualizado

--- db/migrate/20260228195049_add_start_date_to_trips.rb
+++
@@ -1,5 +0,0 @@
-class AddStartDateToTrips < ActiveRecord::Migration[7.1]
-  def change
-    add_column :trips, :start_date, :date
-  end
-end
--- db/migrate/20260228200154_add_trip_id_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddTripIdToMessages < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :messages, :trip, null: false, foreign_key: true
-  end
-end
