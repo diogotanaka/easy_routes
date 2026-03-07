f60cb93fff66af571c51280a1667bb559b361f34
Thais Almeida Gai <thais.gabriel@gmail.com>
2026-03-07 09:42:54 -0300

    termino da configuração do scroll automático de ajuste no markdown

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,24 +7,11 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
-    @message.trip = @trip
-    @message.role = "user"
+  @message = Message.new(message_params)
+  @message.trip = @trip
+  @message.role = "user"
 
   if @message.save
-    # ai_response = RubyLLM.chat.ask(
-    #   "Você é um assistente de viagens.
-    #   Trip: #{@trip.destination}.
-    #   Mensagem: #{@message.content}.
-    #   Responda com sugestões curtas."
-    # ).content
-
-
-    # Message.create!(
-    #   content: ai_response,
-    #   role: "assistant",
-    #   trip: @trip
-    # )
 
     ruby_llm_chat = RubyLLM.chat
     @trip.messages.each do |message|
@@ -33,27 +20,29 @@ class MessagesController < ApplicationController
 
     ai_response = ruby_llm_chat.with_instructions(assistant_context).ask(@message.content)
 
-    Message.create!(content: ai_response.content, role: "assistant", trip: @trip)
+    @assistant_message = Message.create!(content: ai_response.content, role: "assistant", trip: @trip)
 
-    redirect_to trip_messages_path(@trip)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to trip_messages_path(@trip) }
+    end
 
   else
-    @messages = @trip.messages.order(:created_at)
-    render :index, status: :unprocessable_entity
+    render "trips/show", status: :unprocessable_entity
   end
 end
 
-  private
+private
 
-  def build_conversation_history
-    @ai_response.each do |message|
-      @ruby_llm_chat.add_message(message)
-    end
-  end
+  # def build_conversation_history
+  #   @ai_response.each do |message|
+  #     @ruby_llm_chat.add_message(message)
+  #   end
+  # end
 
   def assistant_context
     "Você é um assistent de viagens local e tem as melhores recomendações com base nos interesses do usuário.
-      O destino de #{@trip.user.first_name}} é #{@trip.destination}, a origem é #{@trip.origin}, porém a cada destino ele pode mudar a origem e o destino conforme suas sugestões.
+      O destino de #{@trip.user.first_name} é #{@trip.destination}, a origem é #{@trip.origin}, porém a cada destino ele pode mudar a origem e o destino conforme suas sugestões.
       Responda com sugestões curtas mas diga os endereços exatos."
   end
 
--- app/javascript/controllers/autoscroll_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  scroll() {
-    console.log("scroll executou")
-    this.element.scrollTop = this.element.scrollHeight
-  }
-}
